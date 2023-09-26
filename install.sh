16dfefef7276607dc4f26080c02ea7277911d0c3
David Allan <david.allan@oracle.com>
2023-09-26 14:43:35 -0700

    new file:   install.sh 	new file:   src/tasks/cloudagentcmd.json 	new file:   src/tasks/containerinstance.json 	new file:   src/tasks/datasciencejob.json 	new file:   src/tasks/datasciencepipeline.json 	new file:   src/tasks/deletecontainerinstance.json 	new file:   src/tasks/pythoncontainerinstance.json 	new file:   src/tasks/shellcontainerinstance.json

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Enter the project key and the workspace
+#export PROJECTKEY=
+#export WORKSPACE_ID=
+#export REGION=
+
+for FILE in src/tasks/*; do 
+  rslt=$(cat $FILE | sed 's/{{PROJECT_KEY}}/'$PROJECTKEY'/g');
+  oci raw-request --http-method POST --target-uri https://dataintegration.${REGION}.oci.oraclecloud.com/20200430/workspaces/$WORKSPACE_ID/tasks --request-body "$rslt";
+done
