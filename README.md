d57e207d2fc269bba5dfa2aa1790ebfa88518a20
David Allan <david.allan@oracle.com>
2023-09-27 07:57:50 -0700

    modified:   README.md 	modified:   install.sh 	new file:   src/tasks/aidocument/aidocumenttable.json 	new file:   src/tasks/aispeech/aispeech.json 	new file:   src/tasks/aivision/aiimageclassification.json 	new file:   src/tasks/aivision/aiimageobjectdetection.json 	new file:   src/tasks/aivision/aiimagetabledetection.json 	new file:   src/tasks/aivision/aiimagetextdetection.json 	renamed:    src/tasks/cloudagentcmd.json -> src/tasks/compute/cloudagentcmd.json 	renamed:    src/tasks/containerinstance.json -> src/tasks/containerinstance/containerinstance.json 	renamed:    src/tasks/deletecontainerinstance.json -> src/tasks/containerinstance/deletecontainerinstance.json 	renamed:    src/tasks/pythoncontainerinstance.json -> src/tasks/containerinstance/pythoncontainerinstance.json 	renamed:    src/tasks/shellcontainerinstance.json -> src/tasks/containerinstance/shellcontainerinstance.json 	new file:   src/tasks/dataflow/rundataflow.json 	renamed:    src/tasks/datasciencejob.json -> src/tasks/datascience/datasciencejob.json 	renamed:    src/tasks/datasciencepipeline.json -> src/tasks/datascience/datasciencepipeline.json 	new file:   src/tasks/monitoring/servicemetrics.json 	new file:   src/tasks/nosql/createtable.json 	new file:   src/tasks/nosql/deleterow.json 	new file:   src/tasks/nosql/getrow.json 	new file:   src/tasks/nosql/updaterow.json 	new file:   src/tasks/nosql/waitondata.json 	new file:   src/tasks/notification/notification.json 	new file:   src/tasks/objectstorage/oscopyobject.json 	new file:   src/tasks/objectstorage/oscreatepar.json 	new file:   src/tasks/objectstorage/osdeleteobject.json 	new file:   src/tasks/objectstorage/oslistobjects.json 	new file:   src/tasks/objectstorage/osrenameobject.json

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # oci_di_utilities
+
+
+# Setup
+Define the following environment variables and then execute the script to create the tasks in OCI DI.  Get the project key and the workspace and the region and set;
+export PROJECTKEY=
+export WORKSPACE_ID=
+export REGION=us-ashburn-1
+
+# Install
+install.sh
+
--- install.sh
+++ install.sh
@@ -5,7 +5,8 @@
 #export WORKSPACE_ID=
 #export REGION=
 
-for FILE in src/tasks/*; do 
+for FILE in src/tasks/*/*; do 
+  echo $FILE
   rslt=$(cat $FILE | sed 's/{{PROJECT_KEY}}/'$PROJECTKEY'/g');
   oci raw-request --http-method POST --target-uri https://dataintegration.${REGION}.oci.oraclecloud.com/20200430/workspaces/$WORKSPACE_ID/tasks --request-body "$rslt";
 done
